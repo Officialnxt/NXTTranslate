521603c55467acf214e130e47b700c6cbd9f4003
Officialnxt <esmith233@textfree.us>
2013-09-21 19:22:35 -0400

    Create fr.php

---
+++ fr.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang = array(
+	'Home' => "Page d'accueil",
+	'About' => 'sur',
+	'Search' => 'Rechercher',
+	'Donate' => 'Faire un don',
+	'Contact' => 'contacter',
+	'Search for products from sites such as' => 'SRecherche de produits provenant de sites tels que',
+	'You can also scan this QR code to donate' => 'Vous pouvez également scanner ce code QR pour faire un don',
+	'nxt' => "est un moteur de recherche qui vous permet de rechercher des produits à partir de nombreux marchands de bitcoin à la fois. NXT n'enregistre pas votre informatiom personnels, les liens sur lesquels vous cliquez, ou ce que vous achetez. NXT soutient et est principalement financé par",
+	'email' => 'Vous pouvez nous envoyer un courriel à',
+	'bitcoin' => 'Bitcoin utilise une technologie pair à pair pour fonctionner sans autorité centrale. Le traitement des transactions et la création des bitcoins est prise en charge collectivement par le réseau. Bitcoin est open-source, son fonctionnement est public, personne ne possède ni ne contrôle Bitcoin et le réseau est ouvert à tous. Grâce à ses propriétés uniques, Bitcoin rend possible des usages prometteurs qui ne pouvaient pas être couverts par les précédents systèmes de paiement.',
+	'results' => 'Plus de résultats de'
+);
+
+?>
