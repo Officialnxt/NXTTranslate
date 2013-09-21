197137de7a91b67655c3954798f53fd8b5318b53
Officialnxt <esmith233@textfree.us>
2013-09-21 19:21:27 -0400

    Create en.php

---
+++ en.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang = array(
+	'Home' => 'Home',
+	'About' => 'About',
+	'Search' => 'Search',
+	'Donate' => 'Donate',
+	'Contact' => 'Contact',
+	'Search for products from sites such as' => 'Search for products from sites such as',
+	'You can also scan this QR code to donate' => 'You can also scan this QR code to donate',
+	'nxt' => 'NXT is a search engine that allows you to search for products from many different bitcoin merchants at once. NXT does not record your personal informatiom, the links you click, or what you purchase. NXT also supports and is mainly funded by',
+	'email' => 'You can email us at',
+	'bitcoin' => 'Bitcoin uses peer-to-peer technology to operate with no central authority or banks; managing transactions and the issuing of bitcoins is carried out collectively by the network. Bitcoin is open-source; its design is public, nobody owns or controls Bitcoin and everyone can take part. Through many of its unique properties, Bitcoin allows exciting uses that could not be covered by any previous payment system.',
+	'results' => 'See more results from'
+);
+
+?>
