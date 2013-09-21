377fb015c08ffabe3c574fa0f1fa215bce7697ce
Officialnxt <esmith233@textfree.us>
2013-09-21 19:26:47 -0400

    Create example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+// Get the users language
+$la = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+$filename = "../$la.php";
+//Check if the file for that language exists
+if (file_exists($filename)) {
+     // If it exists, require that file
+     require($filename);
+}else{
+    //If it doesn't exist, use english.
+     require("en.php");
+}
+
+echo $lang['Home'];
+
+echo $lang['About'];
+
+echo $lang['Search'];
+?>
