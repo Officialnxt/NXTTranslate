b840a0bae57625ce937de0b76ce8f56ce899e885
Officialnxt <esmith233@textfree.us>
2013-09-21 19:22:13 -0400

    Create es.php

---
+++ es.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang = array(
+	'Home' => 'Inicio',
+	'About' => 'acerca de',
+	'Search' => 'búsqueda',
+	'Donate' => 'Donar',
+	'Contact' => 'contacto',
+	'Search for products from sites such as' => 'Busca productos de sitios como',
+	'You can also scan this QR code to donate' => 'También puede escanear el código QR ​​a donar',
+	'nxt' => 'es un motor de búsqueda que le permite buscar productos de muchos comerciantes diferentes bitcoin a la vez. NXT no registra su informatiom personal, los enlaces que utiliza, o lo que usted compra. NXT también apoya y se financia principalmente con',
+	'email' => 'Usted puede enviarnos un email a',
+	'bitcoin' => 'Bitcoin utiliza tecnología punto a punto para operar sin una autoridad central; gestionando las transacciones y la emisión de Bitcoins que se llevan a cabo conjuntamente por la red. A través de muchas de sus propiedades únicas, Bitcoin permite usos interesantes que no pueden ser cubiertos por otros sistemas de pago.',
+	'results' => 'Más resultados de'
+);
+
+?>
